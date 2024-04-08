112dbb8ec405c72b480321a5dcc7021840b967f6
Sebastian Castwall <Sebastian.castwall@gmail.com>
2024-04-08 07:18:27 +0200

    worksrename

--- renameFiles.js
+++ renameFiles.js
@@ -1,25 +1,33 @@
 const fs = require('fs');
 const path = require('path');
 
-const buildDirectory = path.join(__dirname, 'build', 'static', 'js');
+const buildDirectoryJs = path.join(__dirname, 'build', 'static', 'js');
+const buildDirectoryCss = path.join(__dirname, 'build', 'static', 'css');
 
-fs.readdir(buildDirectory, (err, files) => {
-  if (err) {
-    console.error('Could not list the directory.', err);
-    process.exit(1);
-  }
-
-  files.forEach((file, index) => {
-    const filePath = path.join(buildDirectory, file);
-    if (file.startsWith('main.') && file.endsWith('.js')) {
-      const newFilePath = path.join(buildDirectory, 'main.js');
-      fs.rename(filePath, newFilePath, function (err) {
-        if (err) {
-          console.error('Could not rename the file.', err);
-          process.exit(1);
-        }
-        console.log(`Renamed ${file} to main.js`);
-      });
+const renameFiles = (directory, oldExt, newExt) => {
+  fs.readdir(directory, (err, files) => {
+    if (err) {
+      console.error(`Could not list the directory: ${directory}`, err);
+      process.exit(1);
     }
+
+    files.forEach((file) => {
+      if (file.startsWith('main.') && file.endsWith(oldExt)) {
+        const filePath = path.join(directory, file);
+        const newFilePath = path.join(directory, `main${newExt}`);
+        fs.rename(filePath, newFilePath, function (err) {
+          if (err) {
+            console.error(`Could not rename the file: ${file}`, err);
+            process.exit(1);
+          }
+          console.log(`Renamed ${file} to main${newExt}`);
+        });
+      }
+    });
   });
-});
+};
+
+// Rename JavaScript files
+renameFiles(buildDirectoryJs, '.js', '.js');
+// Rename CSS files
+renameFiles(buildDirectoryCss, '.css', '.css');
