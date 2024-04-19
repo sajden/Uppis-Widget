ca605ef02a859ef83f3465d8b3b47c425d203219
Sebastian Castwall <Sebastian.castwall@gmail.com>
2024-04-19 16:56:58 +0200

    secrets

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,21 +1,18 @@
-// Import the functions you need from the SDKs you need
+// Import the necessary Firebase modules
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Updated Firebase configuration using environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyBP6An4NorfM6D93IhozF6BXdBVF0b8BQ0",
-  authDomain: "uppis-908bd.firebaseapp.com",
-  projectId: "uppis-908bd",
-  storageBucket: "uppis-908bd.appspot.com",
-  messagingSenderId: "789849971653",
-  appId: "1:789849971653:web:3112f81aa6e644d4adc9ac",
-  measurementId: "G-S49D9M7T72"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
@@ -24,4 +21,5 @@ const firestore = getFirestore(app);
 const auth = getAuth(app);
 const storage = getStorage(app);
 
-export { firestore, auth, storage}; // Export needed Firebase services
+// Export the initialized Firebase services
+export { firestore, auth, storage };
